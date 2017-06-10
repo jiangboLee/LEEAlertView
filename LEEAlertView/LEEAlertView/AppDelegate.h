15cf63d5dc52d2bca487b26193f7bd5cd054bd98
李江波 <584556825@qq.com>
2017-06-10 23:28:03 +0800

    配置podspec

---
+++ LEEAlertView/LEEAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LEEAlertView
+//
+//  Created by 李江波 on 2017/6/10.
+//  Copyright © 2017年 lijiangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
